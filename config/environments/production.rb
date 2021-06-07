7313384a5524961a758ff4d66540d9c63572ec63
chomchompanic <chomurashouta@gmail.com>
2021-06-07 21:03:04 +0900

    modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :user_name => Rails.application.credentials.g_mail[:g_mail], # 送信先アドレス
+      :password => Rails.application.credentials.g_mail[:g_pass], #2段階認証パスワード
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
