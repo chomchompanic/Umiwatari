e1b05326895a3f79aa2052ae31b6927e6c8af2fb
chomchompanic <chomurashouta@gmail.com>
2021-06-11 16:16:27 +0900

    delete sendgrid-page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = "#{ENV['HEROKU_APPNAME']}.herokuapp.com"
   config.action_mailer.smtp_settings = {
